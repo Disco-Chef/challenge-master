b3c515fa806dfebfcd22b4a2959dd5a3a91d36c7
Nikos Agathos <nicolas.agathos@gmail.com>
2020-03-05 17:28:24 +0100

    had it working with cors a while ago, now it is bugging again, time to debug

--- api/app.rb
+++ api/app.rb
@@ -1,27 +1,52 @@
 require 'sinatra'
 require 'sinatra/cross_origin'
+require 'open-uri'
+require 'byebug'
+require 'date'
 configure do
   enable :cross_origin
 end
 
 before do
-  response.headers["Access-Control-Allow-Origin"] = "*"
+  response.headers['Access-Control-Allow-Origin'] = '*'
 end
 
 # ROUTES ...
-get '/v1/indexations' do
-  "Hello World"
-end
+
  post '/v1/indexations' do
   data = JSON.parse request.body.read
-  print data
-  { data: "Hello #{data['name']}!" }.to_json
+
+  signed_on = Date.parse(data.signed)
+  signed_on = signed_on - 31
+  base_month = signed_on.strftime("%Y-%m")
+
+  start_on = Date.parse(data.start)
+  testy = Date.parse(start_on.strftime("%d-%m-") + Date.today.year)
+  testy = Date.parse(start_on.strftime("%d-%m-") + (Date.today.year - 1)) if testy > Date.today
+
+  current_month = testy.strftime("%Y-%m")
+
+  bases = [2013, 2004, 1996, 1988]
+
+  bases.each do |year|
+    return base_year = year if year <= signed_on.year
+  end
+
+  rent = data.rent
+  region = data.region
+  print call_endpoint(base_year, base_month)
 end
 
-options "*" do
-  response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
-  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
-  response.headers["Access-Control-Allow-Origin"] = "*"
+options '*' do
+  response.headers['Allow'] = 'GET, PUT, POST, DELETE, OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token'
+  response.headers['Access-Control-Allow-Origin'] = '*'
   200
 end
 
+def call_endpoint(base, base_month)
+  url = "https://fi7661d6o4.execute-api.eu-central-1.amazonaws.com/prod/indexes/#{base}/#{base_month}"
+  index_doc = open(url)
+  return index_doc
+  # byebug
+end
