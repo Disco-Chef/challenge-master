89e1002e315e09ad530e84f5b00f918d209fb4f2
Nikos Agathos <nicolas.agathos@gmail.com>
2020-03-05 14:09:52 +0100

    form inputs ok (pesky radio buttons for later), onto a table of past inputs. Could be useful for user to see previous inputs and results (mostly useful for me though)

---
+++ web/vue-app/src/components/IndexationForm.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="indexation-form">
+    <form @submit.prevent="handleSubmit">
+      <div class="groups">
+        <div class="group">
+          <label>Contract Start Date:</label>
+          <input
+            ref="first"
+            type="text"
+            v-model="input.start"
+          />
+        </div>
+        <div class="group">
+          <label>Contract Signed On:</label>
+          <input
+            type="text"
+            v-model="input.signed"
+          />
+        </div>
+      </div>
+      <div class="groups">
+        <div class="group">
+          <label>Rent (Euro):</label>
+          <input
+            type="text"
+            v-model="input.rent"
+          />
+        </div>
+        <div class="group">
+          <label>Region (brussels, flanders, wallonia):</label>
+          <input
+            type="text"
+            v-model="input.region"
+          />
+        </div>
+      </div>
+
+      <!-- Big fail trying with radio buttons -->
+      <!--  <label for="region">Brussels</label>
+      <input type="radio" id="brussels" value="brussels" v-model="region">
+      <label for="region">Flanders</label>
+      <input type="radio" id="flanders" value="flanders" v-model="region">
+      <label for="region">Wallonia</label>
+      <input type="radio" id="wallonia" value="wallonia" v-model="region">
+      <br>
+      <span>Region: {{ region }}</span> -->
+      <button>Compute indexed rent!</button>
+
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'indexation-form',
+    data() {
+      return {
+        submitting: false,
+        error: false,
+        success: false,
+        input: {
+          start: '',
+          signed: '',
+          rent: '',
+          region: ''
+        },
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+
+  .groups {
+    display: flex;
+    justify-content: space-between;
+  }
+  input {
+    width: 150px;
+  }
+  label {
+    width: 150px;
+  }
+</style>
